cad78718dab699cf184af68b99e07310c9bf1d0a
ndpalmer <ndpalmer@gmail.com>
2021-09-14 12:19:55 +1000

    new file:   eks-cluster.tf 	new file:   k8s.tf 	new file:   kubeconfig_my-eks-cluster 	new file:   outputs.tf 	new file:   security-groups.tf 	new file:   terraform.tfstate 	new file:   terraform.tfstate.backup 	new file:   versions.tf 	new file:   vpc.tf

---
+++ versions.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.58.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.4.1"
+    }
+  }
+
+  required_version = "> 0.14"
+}
